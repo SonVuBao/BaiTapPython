268ce94e5c1ef6cdfcfa7cf8b65b62ffc824afd7
SonVuBao <42171094+SonVuBao@users.noreply.github.com>
2018-08-09 09:40:55 +0700

    Update Bai4.py

--- Bai4.py
+++ Bai4.py
@@ -4,25 +4,24 @@ print ("nhap n")
 n = int(input())
 i=1
 x= n/2
-Sum1 =0
-Sum2 =0
+Sum =0
 #thread 1 tính từ 1-n/2
 def Thread1():
    global i
-   global Sum1
+   global Sum
    while i<n/2:
-      Sum1 = Sum1+i
+      Sum = Sum+i
       i=i+1
 
-   print("Thread1:", Sum1)
+   print("Thread1:", Sum)
 #thread 2 tính từ n/2-n
 def Thread2():
    global x
-   global Sum2
+   global Sum
    while x<=n:
-      Sum2= Sum2 + x
+      Sum= Sum + x
       x= x +1
-   print("Thread2:",Sum2)
+   print("Thread2:",Sum)
 
 #Gắn hành động cho các thread
 thread1 = threading.Thread(target=Thread1)
@@ -35,5 +34,5 @@ thread2.start()
 #thread 1 và thread 2 bị ngắt
 thread1.join()
 thread2.join()
-print("Tong",Sum1 + Sum2)
+print("Tong",Sum)
 
