f95d70809a72388e1b5f166695d07470d02fb00c
SonVuBao <42171094+SonVuBao@users.noreply.github.com>
2018-08-09 09:43:56 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -1,10 +1,12 @@
 import random
 qty = 1000
-randPoints = []
+randPoints = [(131,328)]
 i = 0
+#lấy random các điểm nằm trong mặt phẳng lặp đi lặp lại 1000 lần 
 while i<qty:
     x = random.randint(0,300)
     y = random.randint(0,400)
+   	#nếu điểm random mời trùng với random cũ thì random lại cho đến khi không lặp
     if (x,y) not in randPoints:
     	randPoints.append((x,y))
     	i += 1
