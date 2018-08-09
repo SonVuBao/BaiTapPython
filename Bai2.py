ecfeb08627bbf3347576d445d771fdde5ddc4a67
SonVuBao <42171094+SonVuBao@users.noreply.github.com>
2018-08-09 09:43:12 +0700

    Update Bai2.py

--- Bai2.py
+++ Bai2.py
@@ -1,5 +1,7 @@
 from collections import Counter
-file=open("C:\\Users\\Dell\\Desktop\\New folder\\input.txt","r+")
+#Mở kết nối tới file input.txt
+file=open("input.txt","r+")
+#Đọc file và tách từng từ và đếm số lần xuất hiện của từ
 wordcount= Counter(file.read().split())
 for word in file.read().split():
 	if word not in wordcount:
@@ -7,6 +9,7 @@ for word in file.read().split():
 	else:
 	    wordcount[word] +=1
 file.close()
+#Tạo file output.txt và lưu kết quả vào đó
 fo = open("output.txt","w+")
 fo.write(str(wordcount))
 fo.close()
