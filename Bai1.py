76951ee44f268845462a480e59940fab4112d5d0
SonVuBao <42171094+SonVuBao@users.noreply.github.com>
2018-08-09 15:49:56 +0700

    Update Bai1.py

--- Bai1.py
+++ Bai1.py
@@ -2,13 +2,12 @@ print ("nhap n")
 #nhập n
 n= int(input())
 i = 2
-#Kiểm tra xem số nào trong mảng là số nguyên tố bằng cách chia cho các số bé hơn số đó 
-while i < n:
-    j = 2
-    while j <= i//2:
-        if i % j == 0:
-            break
-        j = j + 1
-    else:
-        print (i)
-    i = i + 1 
+#Kiểm tra xem số nào là số nguyên tố
+while(i < n):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j): break
+      j = j + 1
+   if (j > i/j) : 
+   	print (i)
+   i = i + 1
